e82dfe1d28de24bd2aea3894946d20446c16bd67
h8rry <harrychenca@gmail.com>
2024-01-19 12:27:15 +0800

    Fixes

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -3,53 +3,58 @@ import $ from 'jquery';
 import bootstrapBundle from "bootstrap/dist/js/bootstrap.bundle.js";
 
 import {
-    indexTasks,
-    postTask,
-    destroy,
-    mark_active,
-    mark_complete,
+  indexTasks,
+  postTask,
+  destroy,
+  mark_active,
+  mark_complete,
 } from './requests.js';
 
 console.log('index.js is loaded');
 
 const renderTasks = function (tasks) {
-    indexTasks(function (response) {
-        console.log('All tasks', response);
+  indexTasks(function (response) {
+    console.log('All tasks', response);
 
-        var htmlString = ''
+    var htmlString = ''
 
-        response.tasks.map(function(task) {
-            htmlString += '<div class="row"><p class="col">' + task.content + '</p><p class="col"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + ' style="margin-right: 15px;"><button class="btn btn-sm btn-outline-primary delete" data-id="' + task.id + '"> Delete Task </button></p>';
-        });
-        
-
-        
-
-        $("#tasks").html(htmlString);
+    response.tasks.map(function (task) {
+      htmlString += '<div class="row"><p class="col">' + task.content + '</p><p class="col"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + ' style="margin-right: 15px;"><button class="btn btn-sm btn-outline-primary delete" data-id="' + task.id + '"> Delete Task </button></p>';
     });
+
+    $("#tasks").html(htmlString);
+  });
 }
 
 $(document).ready(function () {
-    console.log('The DOM is ready');
+  console.log('The DOM is ready');
+  renderTasks();
 
-    renderTasks();
-    $(document).on('click', '.delete', function() {
-        destroy($(this).data('id'));
-        renderTasks();
-        console.log("This should be deleted");
+  $(document).on('click', '.delete', function () {
+    destroy($(this).data('id'), function (response) {
+      renderTasks();
     });
 
+    console.log("This should be deleted");
+  });
 
-    $('#add-tasks').on('submit', function (event) {
-        event.preventDefault();
-    
-        console.log('The form is being submitted');
-    
-        postTask($("#new-task-additions").val(), function (response) {
-            console.log(response);
-            renderTasks();
-        });
+  $('#add-tasks').on('submit', function (event) {
+    event.preventDefault();
+
+    console.log('The form is being submitted');
+
+    postTask($("#new-task-additions").val(), function (response) {
+      console.log(response);
+      renderTasks();
     });
+  });
+
+  // Add mark-complete
+  $(document).on('click', '.mark-complete', function () {
+    mark_complete($(this).data('id'), function (response) {
+      renderTasks();
+    });
+  });
 })
 
 
